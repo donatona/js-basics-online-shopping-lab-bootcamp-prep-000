6c79a2280a71c9b6bcc38695eb57d74cdbd0e6c6
Tona Morales-Calkins <tona.mc@gmail.com>
2018-10-14 19:20:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function setCart(c) {
 
 function addToCart(item) {
 //need a way to create the Object so this function passes the test (Error: Expected [Function: String] to equal [Function: Object]  + expected - actual) --- as I understand the test file the program tests for an Object in the cart before allowing me to turn the value of the Object into a String, which will be returned as a part of the sentence below. I've got the String value, as the Error code shows. Now I need an Object before it!
-item = itemConstructor;
+
 getCart().push(item);
 return `${item.itemName} has been added to your cart.`;
 }
