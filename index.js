f1b6304135b8c00201cda79cd3f60ba96650c11e
Tona Morales-Calkins <tona.mc@gmail.com>
2018-10-14 17:59:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function setCart(c) {
 function addToCart(item) {
 
 getCart().push(item);
-return `${newItem.itemName} has been added to your cart.`;
+return `${item.itemName} has been added to your cart.`;
 }
 
 function viewCart() {
