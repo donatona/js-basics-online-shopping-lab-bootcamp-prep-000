77e326a1b41ada3dc825f0b3674ed91edce4101e
Tona Morales-Calkins <tona.mc@gmail.com>
2018-10-14 19:14:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ function setCart(c) {
 }
 
 function addToCart(item) {
-//need a way to create the Object so this function passes the test 
+// //need a way to create the Object so this function passes the test (Error: Expected [Function: String] to equal [Function: Object]
+//       + expected - actual)
 getCart().push(item);
 return `${item.itemName} has been added to your cart.`;
 }
